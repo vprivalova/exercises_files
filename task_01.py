e7ef7022fe61924ab37048f111ca2dc973fa5b90
vprivalova <privalovaa.vv@gmail.com>
2023-10-09 19:38:23 +0700

    commit task 01

--- task_01.py
+++ task_01.py
@@ -1,7 +1,5 @@
-f1 = open('input.txt', 'r')
-f2 = open('output.txt', 'w')
+with open('input.txt', 'r') as f1:
+    result = (f1.read()).upper()
 
-f2.write((f1.read()).upper())
-
-f1.close()
-f2.close()
+with open('output.txt', 'w') as f2:
+    f2.write(result)
